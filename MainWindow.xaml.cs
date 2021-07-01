aab18b7b12a8c16001bb66337e452de87e3eac15
Mariana Licona <m.licona@abtis.de>
2021-07-01 02:42:02 +0200

    add method to shuffle and finished program

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,5 +29,20 @@ namespace PickACardUI
         {
 
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] pickedCards = CardPicker.PickSomeCards((int) numberOfCards.Value);
+            listOfCards.Items.Clear();
+            foreach (string card in pickedCards)
+            {
+                listOfCards.Items.Add(card);
+            }
+        }
     }
 }
